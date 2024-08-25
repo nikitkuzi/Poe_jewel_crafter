4d6faca6ca00a2b2faea1e96b2e3924a8f897f8d
nikitkuzi <nikitkuzi@gmail.com>
2024-08-25 18:22:59 -0400

    added check click with regex

--- clicker.py
+++ clicker.py
@@ -1,6 +1,10 @@
+import re
+
 import keyboard
 import mouse
 import time
+import json
+import clipboard
 
 
 def get_coords():
@@ -29,25 +33,35 @@ def loop_click(alt, aug, item):
     use(item)
 
 
+def check_click_with_regex(item, regex):
+    mouse.move(*item)
+    keyboard.send("ctrl+alt+c")
+    time.sleep(0.1)
+    clip = clipboard.paste()
+    for reg in regex:
+        if reg in clip:
+            return True
+    return False
+
+def check_min_amont(*args):
+    mn = 99999999
+    reg = re.Pattern()
+    for item in args:
+        mouse.move(*item)
+        keyboard.send("ctrl+c")
+
+
+
 if __name__ == '__main__':
-    with open("pos_for_clicker.txt") as f:
-        print(f.readline())
-        print(f.readline())
-        print(f.readline())
-        exit(0)
-        alt = [int(a) for a in f.readline()[1:-2].split(",")]
-        item = [int(a) for a in f.readline()[1:-2].split(",")]
-        aug = [int(a) for a in f.readline()[1:-1].split(",")]
-        print(alt, item, aug)
-    # while True:
+    with open("pos_for_clicker.json") as f:
+        file = json.load(f)
+    with open("regex_to_find.txt") as f:
+        regex = f.read().split('\n')
+    alt = file['currency tab']['alt']
+    aug = file['currency tab']['aug']
+    item = file['currency tab']['item']
+
     time.sleep(4)
-    # while True:
-    # take(alt)
-    # use(item)
-    # take(aug)
-    # use(item)
-    i = 0
-    mouse.on_click(loop_click, (alt, aug, item))
-    while i < 10:
-        time.sleep(0.5)
-        i += 1
+
+
+
