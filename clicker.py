c909cd494cef33b492097a4e19a69d2a0392b944
nikitkuzi <nikitkuzi@gmail.com>
2024-08-29 20:34:44 -0400

    fixed regex

--- clicker.py
+++ clicker.py
@@ -108,10 +108,10 @@ if __name__ == '__main__':
     alt = file['currency tab']['alt']
     aug = file['currency tab']['aug']
     item_in_currency = file['currency tab']['item']
-    # pos_needed = ['pos0', 'pos2', 'pos10', 'pos12', 'pos20', 'pos22', 'pos30', 'pos32', 'pos40', 'pos42', 'pos50', 'pos52']
-    pos_needed = ['pos0', 'pos2']
+    pos_needed = ['pos0', 'pos2', 'pos10', 'pos12', 'pos20', 'pos22', 'pos30', 'pos32', 'pos40', 'pos42', 'pos50', 'pos52']
+    # pos_needed = ['pos0', 'pos2']
     items_in_inventory = [file['inventory'][pos] for pos in pos_needed]
     time.sleep(4)
     print("start")
-    run_alt_spam(alt, aug, item_in_currency, regex)
-    # run_helmet_spam(alt, aug, items_in_inventory, regex)
+    # run_alt_spam(alt, aug, item_in_currency, regex)
+    run_helmet_spam(alt, aug, items_in_inventory, regex)
