714ba4981a96dcd611e61350b23847ca4eb465a6
nikitkuzi <nikitkuzi@gmail.com>
2024-10-18 17:01:41 -0400

    added essence tab and refactor

--- clicker.py
+++ clicker.py
@@ -10,8 +10,11 @@ import pyperclip
 
 
 def get_coords():
-    print(mouse.get_position())
-    # return mouse.get_position()
+    while True:
+        mouse.wait(button="left", target_types="down")
+        print(mouse.get_position())
+        if keyboard.is_pressed("space"):
+            return
 
 
 def take(coords):
@@ -52,9 +55,12 @@ def check_click_with_regex(item, regex):
     return False
 
 
-def check_min_amont(*args):
+
+
+def check_min_amont(args):
     mn = 99999999
-    reg = '(Stack Size: )\d,?\d*\/\d.?\d+'
+    # reg = '(Stack Size: )\d,?\d*\/\d.?\d+'
+    reg = '(Stack Size: )\d,?\d*\/\d+'
     for item in args:
         mouse.move(item[0], item[1])
         time.sleep(0.11)
@@ -68,74 +74,81 @@ def check_min_amont(*args):
     return mn
 
 
-def run_currency_spam(currency1, currency2, item_in_currency, regex, check=True):
+def run_currency_spam(currency: list[str], tab: str, regex: list[str], check: bool = True,
+                      pos_for_clicker: dict = None) -> None:
+    """
+    Perform a currency spam on an item in a tab.
+    """
     i = 0
-    mn = check_min_amont(currency1, currency2)
+    currency_converted = [pos_for_clicker[tab][current] for current in currency]
+    item_pos = pos_for_clicker[tab]['item']
+    mn = check_min_amont(currency_converted)
     if check:
         while i < mn:
             if keyboard.is_pressed("space"):
                 break
-            loop_click(currency1, item_in_currency)
-            if check_click_with_regex(item_in_currency, regex):
-                break
-            loop_click(currency2, item_in_currency)
-            if check_click_with_regex(item_in_currency, regex):
-                break
-            i += 1
+            for cur in currency_converted:
+                loop_click(cur, item_pos)
+                if check_click_with_regex(item_pos, regex):
+                    break
+                i += 1
     else:
         while i < mn:
             if keyboard.is_pressed("space"):
                 break
-            loop_click(currency1, item_in_currency)
-            loop_click(currency2, item_in_currency)
-            i += 1
+            for cur in currency_converted:
+                loop_click(cur, item_pos)
+                i += 1
 
 
-def run_inventory_spam(alt, aug, items, regex):
+def run_inventory_spam(currency: list[str], tab: str, items: list[list[str, str]], regex: list[str],
+                       check: bool = True):
+    """
+    Performs currency spam in the inventory from selected tab at selected positions
+    """
     item = 0
     i = 0
-    mn = check_min_amont(alt, aug)
-    while i < mn and item < len(items):
-        if keyboard.is_pressed("space"):
-            break
-        loop_click(alt, items[item])
-        if check_click_with_regex(items[item], regex):
-            item += 1
-            i += 1
-            time.sleep(1)
-            continue
-        loop_click(aug, items[item])
-        if check_click_with_regex(items[item], regex):
-            item += 1
-            i += 1
-            time.sleep(1)
-            continue
-        i += 1
+    currency_converted = [pos_for_clicker[tab][current] for current in currency]
+    mn = check_min_amont(currency_converted)
+    if check:
+        while i < mn and item < len(items):
+            if keyboard.is_pressed("space"):
+                break
+            for cur in currency_converted:
+                loop_click(cur, items[item])
+                if check_click_with_regex(items[item], regex):
+                    item += 1
+                    i += 1
+                    time.sleep(1)
+                    continue
+                i += 1
+    else:
+        while i < mn:
+            if keyboard.is_pressed("space"):
+                break
+            for cur in currency_converted:
+                loop_click(cur, items[item])
+                item = (item + 1) % len(items)
+                i += 1
 
 
 if __name__ == '__main__':
     with open("pos_for_clicker.json") as f:
-        file = json.load(f)
+        pos_for_clicker = json.load(f)
     with open("regex_to_find.txt") as f:
         regex = f.read().split('\n')
 
-    # x, y pairs
-    alt = file['currency tab']['alt']
-    aug = file['currency tab']['aug']
-    chance = file['currency tab']['chance']
-    scour = file['currency tab']['scour']
-
-    item_in_currency = file['currency tab']['item']
     # pos_needed = ['pos0', 'pos2', 'pos10', 'pos12', 'pos20', 'pos22', 'pos30', 'pos32', 'pos40', 'pos42', 'pos50', 'pos52']
     # pos_needed = ['pos0', 'pos5', 'pos10', 'pos15', 'pos20', 'pos25', 'pos30', 'pos35', 'pos40', 'pos45', 'pos50', 'pos55']
-    pos_needed = ['pos0', 'pos5', 'pos10', 'pos15', 'pos20', 'pos25', 'pos30', 'pos35']
+    # pos_needed = ['pos0', 'pos5', 'pos10', 'pos15', 'pos20', 'pos25', 'pos30', 'pos35']
     # pos_needed = ['pos0', 'pos5', 'pos10']
     # pos_needed = ['pos0','pos10','pos20','pos30','pos40','pos50']
     # pos_needed = ['pos0', 'pos2', 'pos10']
-    # pos_needed = ['pos0', 'pos2']
-    items_in_inventory = [file['inventory'][pos] for pos in pos_needed]
+    pos_needed = ['pos0', 'pos5']
+    items_in_inventory = [pos_for_clicker['inventory'][pos] for pos in pos_needed]
     time.sleep(2)
     print("start")
-    # run_currency_spam(chance, scour, item_in_currency, regex, check=False)
-    run_currency_spam(alt, aug, item_in_currency, regex, check=True)
-    # run_inventory_spam(alt, aug, items_in_inventory, regex)
+    # run_currency_spam(["chance", "scour"], item_in_currency, regex, check=False, pos_for_clicker=pos_for_clicker)
+    # run_currency_spam(["alt", "aug"], "currency tab", regex, check=True, pos_for_clicker=pos_for_clicker)
+    run_currency_spam(["Hatred"], "essence tab", regex, check=True, pos_for_clicker=pos_for_clicker)
+    # run_inventory_spam(["alt", "aug"], "currency tab", items_in_inventory, regex, check=False)
